e8d0db1294be17cc00817b92ca6109120bc36313
marksmirnow <mark.smirnow2@gmail.com>
2021-12-19 16:59:03 +0300

    get elements of page

---
+++ app.js
@@ -0,0 +1,5 @@
+// * Get elements
+const startButton = document.querySelector('.button-start-game');
+const gameField = document.querySelector('.game-field');
+const moles = document.querySelectorAll('.mole');
+const holes = document.querySelectorAll('.hole');
