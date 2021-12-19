b41f5091b683c7ea1b79c362d6a6dc3dc6a80150
marksmirnow <mark.smirnow2@gmail.com>
2021-12-19 19:00:32 +0300

    get random time & random hole

--- app.js
+++ app.js
@@ -3,3 +3,18 @@ const startButton = document.querySelector('.button-start-game');
 const gameField = document.querySelector('.game-field');
 const moles = document.querySelectorAll('.mole');
 const holes = document.querySelectorAll('.hole');
+
+
+function getRandomTime(min, max) {
+   return Math.floor((Math.random() * (max - min + 1) + min));
+}
+
+
+function getRandomHole(holes) {
+   const randomIndex = Math.floor(Math.random() * (holes.length));
+   const randomHole = holes[randomIndex];
+   randomHole.style.background = 'blue';
+   return randomHole;
+}
+
+console.log(getRandomHole(holes));
